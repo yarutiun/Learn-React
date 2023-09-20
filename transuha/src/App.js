e215d75b2ebda21b1c42c54691d027334db43c08
yarutiun <y.arutiunofff@gmail.com>
2023-09-20 17:15:38 +0200

    simple counter in React

---
+++ transuha/src/App.js
@@ -0,0 +1,19 @@
+import React, {useState} from 'react';
+import ClassCounter from './components/ClassCounter';
+import Counter from './components/counter';
+
+function App() {
+  // let likes = 0;
+  // const [likes, setLikes] = useState(5);
+  // const[value, setValue] = useState('string');
+
+
+
+  return (
+    <div className='App'>
+      <ClassCounter/>
+    </div>
+  );
+}
+
+export default App;
